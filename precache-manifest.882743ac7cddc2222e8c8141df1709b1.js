0f90e1648411d761b9ae5ccd1523472ee96eca5e
GoglichidzeMike <goglichidze.mike@gmail.com>
2020-11-04 22:10:04 +0400

    Updates

---
+++ precache-manifest.882743ac7cddc2222e8c8141df1709b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99663c3c6a66b8e09da5cb180adec74",
+    "url": "/GoglichidzeMike/Mikegram/index.html"
+  },
+  {
+    "revision": "4828aed51eb4468ab8c2",
+    "url": "/GoglichidzeMike/Mikegram/static/css/main.ae4add26.chunk.css"
+  },
+  {
+    "revision": "0ca4bfc2155b9c4147cf",
+    "url": "/GoglichidzeMike/Mikegram/static/js/2.ae5bed90.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/GoglichidzeMike/Mikegram/static/js/2.ae5bed90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4828aed51eb4468ab8c2",
+    "url": "/GoglichidzeMike/Mikegram/static/js/main.5354aaa6.chunk.js"
+  },
+  {
+    "revision": "ba7966fa82e3ee1ce89e",
+    "url": "/GoglichidzeMike/Mikegram/static/js/runtime-main.bf8ed27f.js"
+  }
+]);
